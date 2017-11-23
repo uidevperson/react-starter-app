a4b805dd85b297bc3b84fad0bb87fd3691247f42
uidevperson <uidevthing@gmail.com>
2017-11-23 18:33:46 +0000

    test

--- src/index.js
+++ src/index.js
@@ -1,6 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import SearchBar from './components/search_bar';
 
-const App = () => <div>hello you</div>;
+const API_KEY = 'AIzaSyDQ2NhKSi9uA2gCDaeipnCnVb0VLI-1D2I';
+
+const App = () => {
+	return (
+		<div>
+			<SearchBar />
+		</div>
+	);
+}
 
 ReactDOM.render(<App />, document.querySelector('.container'));
