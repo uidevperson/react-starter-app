1a476e37fa757b4b2a30f4523f0cae36e2b69826
uidevperson <32483505+uidevperson@users.noreply.github.com>
2017-11-24 15:35:53 +0000

    controlled component

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -3,15 +3,16 @@ import React, {Component} from 'react';
 class SearchBar extends Component {
   constructor(props){
     super(props);
-    this.state = { term: '' };
+    this.state = { term: 'Starting Value' };
   }
 
 	render(){
 		return (
         <div>
-          <input onChange={ e => this.setState( {term: e.target.value}) } />
-          <br />
-          Value of the input: {this.state.term}
+          <input
+          value={this.state.term}
+          onChange={ e => this.setState( {term: e.target.value}) }
+          />
         </div>
       );
 	}
