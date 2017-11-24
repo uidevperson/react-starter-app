66b1d4a61d7a7fa657f9ad057f6ad26ad9b9acbe
uidevperson <32483505+uidevperson@users.noreply.github.com>
2017-11-24 16:22:07 +0000

    introducing props

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import YouTubeSearch from 'youtube-api-search';
 import SearchBar from './components/search_bar';
+import VideoList from './components/video_list';
 const API_KEY = 'AIzaSyDQ2NhKSi9uA2gCDaeipnCnVb0VLI-1D2I';
 
 YouTubeSearch({key: API_KEY, term: 'ron swanson'}, data => console.log(data));
@@ -18,6 +19,7 @@ class App extends Component {
     return (
       <div>
         <SearchBar />
+        <VideoList videos={this.state.videos} />
       </div>
     );
   }
