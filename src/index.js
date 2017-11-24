6359a2a8e423cfda3141845621de78bacf4f1de4
uidevperson <32483505+uidevperson@users.noreply.github.com>
2017-11-24 15:56:35 +0000

    youtube search test

--- src/index.js
+++ src/index.js
@@ -1,8 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import YouTubeSearch from 'youtube-api-search';
 import SearchBar from './components/search_bar';
-
+//
 const API_KEY = 'AIzaSyDQ2NhKSi9uA2gCDaeipnCnVb0VLI-1D2I';
+YouTubeSearch({key: API_KEY, term: 'ron swanson'}, data => console.log(data));
 
 const App = () => {
 	return (
