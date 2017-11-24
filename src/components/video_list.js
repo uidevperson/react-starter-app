0646d54271c50069401893b345ba6e7e77b4b5f6
uidevperson <32483505+uidevperson@users.noreply.github.com>
2017-11-24 16:42:18 +0000

    list with items

--- src/components/video_list.js
+++ src/components/video_list.js
@@ -1,9 +1,14 @@
 import React from 'react';
+import VideoListItem from './video_list_item';
 
 const VideoList = (props) => {
-  return <ul className="col-md-4 list-group">
-    {props.videos.length}
-  </ul>
+  const videoItems = props.videos.map(video => <VideoListItem video={video} /> );
+
+  return (
+    <ul className="col-md-4 list-group">
+      {videoItems}
+    </ul>
+  );
 }
 
 export default VideoList;
