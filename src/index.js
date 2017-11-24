eeb02bddc8cfd38848069f6e3835357c762f6bc1
uidevperson <32483505+uidevperson@users.noreply.github.com>
2017-11-24 16:08:45 +0000

    add initial state and fetch data

--- src/index.js
+++ src/index.js
@@ -1,17 +1,26 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import YouTubeSearch from 'youtube-api-search';
 import SearchBar from './components/search_bar';
-//
 const API_KEY = 'AIzaSyDQ2NhKSi9uA2gCDaeipnCnVb0VLI-1D2I';
+
 YouTubeSearch({key: API_KEY, term: 'ron swanson'}, data => console.log(data));
 
-const App = () => {
-	return (
-		<div>
-			<SearchBar />
-		</div>
-	);
+class App extends Component {
+  constructor(props){
+    super(props)
+    this.state = {videos: []};
+    // do initial fetch of data
+    YouTubeSearch({key: API_KEY, term: 'ron swanson'}, videos => this.setState({ videos }));
+  }
+
+  render(){
+    return (
+      <div>
+        <SearchBar />
+      </div>
+    );
+  }
 }
 
 ReactDOM.render(<App />, document.querySelector('.container'));
