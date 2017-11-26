c4293a6e8567fac360b6e41e4369e9abdb68da0e
uidevperson <uidevthing@gmail.com>
2017-11-26 16:12:00 +0000

    video details

---
+++ src/components/video_detail.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import VideoDetail from './video_detail';
+
+const VideoDetail = ({video}) => {
+  const videoId = video.id.videoId;
+const url = `http://youtube.com/embed/${videoId}`;
+
+  return (
+    <div className="video-detail col-md-8">
+    	<div className="embed-responsive embed-responsive-16by9">
+    		<iframe className="embed-responsive-item" src={url}></iframe>
+    	</div>
+    	<div className="details"></div>
+    		<div>{video.snippet.title}</div>
+    		<div>{video.snippet.description}</div>
+    </div>
+  );
+}
+
+export default VideoDetail;
