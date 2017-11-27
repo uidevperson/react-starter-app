865817f14cc45667f1765a36556c6e8e6095b8ee
uidevperson <32483505+uidevperson@users.noreply.github.com>
2017-11-27 10:59:11 +0000

    typo

--- src/components/video_list_item.js
+++ src/components/video_list_item.js
@@ -11,7 +11,7 @@ const VideoListItem = ({video, onVideoSelect}) => {
           <img className="media-object" src={imageUrl}/>
         </div>
         <div className="media-body">
-          <div className="media-heading">{video.snippet.titlw}</div>
+          <div className="media-heading">{video.snippet.title}</div>
         </div>
       </div>
     </li>
